a8c93a8a6ee0379ca9cbcf2d855f7b684ae3dddf
EspiraMarvin <espiramarvin@gmail.com>
2024-09-11 17:19:34 +0300

    feat: add seeds module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { SeedsModule } from './seeds/seeds.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DATABASE_HOST,
       port: 5432,
-      username: '',
-      password: '',
-      database: 'seed-house',
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [],
       synchronize: true, // for dev purposes only
     }),
     UsersModule,
+    SeedsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
