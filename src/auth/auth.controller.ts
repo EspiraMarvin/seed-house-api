bcc3ff6e617e3f6219ab49ea6366787ef42bcc9e
EspiraMarvin <espiramarvin@gmail.com>
2024-09-22 22:22:57 +0300

    feat: add authenication signin method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/signIn.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  /** sign in */
+  @HttpCode(HttpStatus.OK)
+  @Post('siginin')
+  signin(@Body() body: SignInDto) {
+    try {
+      return this.authService.signIn(body.email, body.password);
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
 }
