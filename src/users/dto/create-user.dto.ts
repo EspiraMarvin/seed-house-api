e77de9cdee01fa2452fa4a90225dc5d3933565be
EspiraMarvin <espiramarvin@gmail.com>
2024-10-01 17:15:44 +0300

    feat: add create user dto
     - adds create user data transfer object

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,39 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export enum UserRole {
+  USER = 'user',
+  ADMIN = 'admin',
+}
+
+export class CreateUserDto {
+  @IsEmail()
+  @MinLength(6)
+  @IsNotEmpty()
+  email: string;
+
+  @MinLength(2)
+  @IsNotEmpty()
+  first_name: string;
+
+  @MinLength(2)
+  @IsNotEmpty()
+  last_name: string;
+
+  @IsString()
+  @IsOptional()
+  phone_number?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsEnum(UserRole, {
+    message: 'Role must be one of the following values: user, admin',
+  })
+  role: UserRole;
+}
