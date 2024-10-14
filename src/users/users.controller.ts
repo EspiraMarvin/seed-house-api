3db8c217206c6a36f1336e16a4ce7a3d7316cb6f
EspiraMarvin <espiramarvin@gmail.com>
2024-10-14 05:10:33 +0300

    feat: add auth & admin guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,16 +7,21 @@ import {
   Param,
   Delete,
   ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AdminGuard } from '../auth/guards/admin.guard';
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UseGuards(AdminGuard)
   create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
     try {
       return this.usersService.create(createUserDto);
@@ -44,7 +49,10 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(
+    @Param('id') id: string,
+    @Body(new ValidationPipe()) updateUserDto: UpdateUserDto,
+  ) {
     try {
       return this.usersService.update(id, updateUserDto);
     } catch (error) {
