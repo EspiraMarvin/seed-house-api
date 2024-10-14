8be5e35847e8f1c213d85ff30e351483bfa87ef1
EspiraMarvin <espiramarvin@gmail.com>
2024-10-14 05:21:15 +0300

    test: add controller tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,8 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
+import { AdminGuard } from '../auth/guards/admin.guard';
+import { JwtService } from '@nestjs/jwt';
 // import * as helper from '../utils/helpers';
 
 describe('UsersController', () => {
@@ -11,7 +13,13 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService, PrismaService, ConfigService],
+      providers: [
+        UsersService,
+        PrismaService,
+        ConfigService,
+        JwtService,
+        AdminGuard,
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
